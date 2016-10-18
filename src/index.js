2cf194c614b2052976a57b653d23b1d751731d0d
John Behan <behanj@gmail.com>
2016-10-18 18:49:51 +0700

    up to 7

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import './css/style.css';
+
+// components
+import App from './components/App';
+import StorePicker from './components/StorePicker';
+
+render(<App/>, document.querySelector('#main'));
